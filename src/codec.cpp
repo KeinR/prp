ffd88ff601c9b8568b0c06df1157c17cdedf0c04
Orion Musselman <orionmusselman@gmail.com>
2020-11-08 21:11:25 +0000

    Migrated all the things to codec

---
+++ src/codec.cpp
@@ -0,0 +1,167 @@
+#include "codec.h"
+
+
+static constexpr char SC_START = '@';
+
+static constexpr char FUNC_FLAG = '@';
+static constexpr char FUNC_OPEN = '[';
+static constexpr char FUNC_CLOSE = ']';
+static constexpr char PAUSE = '\x1F';
+static constexpr char PAUSE_CHR = '#';
+static constexpr char ESCAPE = '\\';
+
+static constexpr char FUNC_BC = '\x10';
+
+static std::string mapShortcode(char g, char v);
+
+bool cd::testEscape(const std::string &in, std::string::size_type i) {
+    return in[i] == ESCAPE && i + 1 < in.size() &&
+        (testFunc(in, i+1) || testPause(in, i+1) || testShortcode(in, i+1));
+}
+void cd::passEscape(const std::string &in, std::string::size_type i) {
+    i++;
+}
+
+bool cd::testPause(const std::string &in, std::string::size_type i) {
+    return in[i] == PAUSE_CHR;
+}
+std::string cd::compilePause(const std::string &in, std::string::size_type &i) {
+    i++;
+    std::string result;
+    result.push_back(PAUSE);
+    return result;
+}
+bool cd::isPause(const std::string &in, std::string::size_type &i) {
+    if (in[i] == PAUSE) {
+        i++;
+        return true;
+    }
+    return false;
+}
+
+bool cd::testFunc(const std::string &in, std::string::size_type i) {
+    return in[i] == FUNC_FLAG &&
+        i + 2 < source.size() &&
+        source[i + 1] == FUNC_OPEN &&
+        // No no-name calls
+        source[i + 2] != FUNC_CLOSE;
+}
+bool cd::testCmpFunc(const std::string &in, std::string::size_type i) {
+    return in[i] == FUNC_BC;
+}
+std::string cd::compileFunc(const std::string &in, std::string::size_type &i) {
+    std::string::size_type mi = i + 2;
+    std::string result;
+    result.push_back(FUNC_BC);
+    for (;; mi++) {
+        if (mi >= in.size()) {
+            return "";
+        } else if (in[mi] == FUNC_CLOSE) {
+            break;
+        }
+        result.push_back(in[mi]);
+    }
+    result.push_back(FUNC_BC);
+    i = mi + 1;
+    return result;
+}
+std::string readFunc(const std::string &in, std::string::size_type &i) {
+    // unlikely to fail
+    std::string::size_type mi = i + 2;
+    std::string result;
+    for (;; mi++) {
+        if (mi >= in.size()) {
+            std::cerr << "WARNING: Failed to read compiled function call" << '\n';
+            return "";
+        } else if (in[mi] == FUNC_BC) {
+            break;
+        }
+        result.push_back(in[mi]);
+    }
+    i = mi + 1;
+    return result;
+}
+
+bool cd::testShortcode(const std::string &in, std::string::size_type i) {
+    return in[i] == SC_START && i + 2 < source.size();
+}
+std::string cd::compileShortcode(const std::string &in, std::string::size_type &i) {
+    std::string mapping = mapShortcode(source[i + 1], source[i + 2]);
+    if (mapping.size() > 0) {
+        // Was successful
+        i += 3;
+    }
+    return mapping;
+}
+
+std::string mapShortcode(char g, char v) {
+    std::string code;
+    switch (g) {
+        case '0':
+            switch (v) {
+                case '0':
+                    code = "0";
+                    break;
+                case '1':
+                    code = "1";
+                    break;
+                case '2':
+                    code = "2";
+                    break;
+                case '3':
+                    code = "3";
+                    break;
+                case '4':
+                    code = "4";
+                    break;
+                case '5':
+                    code = "21";
+                    break;
+                case '6':
+                    code = "9";
+                    break;
+                case '7':
+                    code = "22";
+                    break;
+                case '8':
+                    code = "23";
+                    break;
+                case '9':
+                    code = "24";
+                    break;
+                case 'a':
+                    code = "25";
+                    break;
+            }
+            break;
+        default:
+            if (v >= '0' && v <= '7') {
+                switch (g) {
+                    case '1':
+                        code = "30";
+                        break;
+                    case '2':
+                        code = "90";
+                        break;
+                    case '3':
+                        code = "40";
+                        break;
+                    case '4':
+                        code = "100";
+                        break;
+                }
+                if (code.size()) {
+                    code.back() = v;
+                }
+            }
+            break;
+    }
+    std::string result;
+    if (code.size() > 0) {
+        result += "\e[";
+        result += code;
+        result += "m";
+        return result;
+    }
+    return result;
+}
--- src/codec.h
+++ src/codec.h
@@ -1,11 +1,23 @@
 #ifndef CODEC_H_INCLUDED
 #define CODEC_H_INCLUDED
 
+#include <string>
+
 namespace cd {
-    static constexpr char PAUSE = '\x1F';
-    static constexpr char PAUSE_CHR = '#';
-    static constexpr char SC_START = '@';
-    static constexpr char ESCAPE = '\\';
+    bool testEscape(const std::string &in, std::string::size_type i);
+    void passEscape(const std::string &in, std::string::size_type i);
+
+    bool testPause(const std::string &in, std::string::size_type i);
+    std::string compilePause(const std::string &in, std::string::size_type &i);
+    bool isPause(const std::string &in, std::string::size_type &i);
+
+    bool testFunc(const std::string &in, std::string::size_type i);
+    bool testCmpFunc(const std::string &in, std::string::size_type i);
+    std::string compileFunc(const std::string &in, std::string::size_type &i);
+    std::string readFunc(const std::string &in, std::string::size_type &i);
+
+    bool testShortcode(const std::string &in, std::string::size_type i);
+    std::string compileShortcode(const std::string &in, std::string::size_type &i);
 }
 
 #endif
